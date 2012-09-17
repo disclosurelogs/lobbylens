53ceb41e8da13163b6dd5e164cf34adc80237f70
Maxious <maxious@lambdacomplex.org>
2012-09-17 21:23:45 +1000

    more sigma upgrade work

--- ngapi.gexf.php
+++ ngapi.gexf.php
@@ -1,37 +1,67 @@
 <?php
 include "libs/config.php";
+
+function add_node($id, $label, $parent="") {
+
+          echo "<node id='$id' label=\"".htmlentities($label,ENT_XML1)."\" ".($parent != ""? "pid='$parent'><viz:size value='1'/>":"><viz:size value='2'/>")
+              ."<viz:color b='".rand(0,255)."' g='".rand(0,255)."' r='".rand(0,255)."'/>"
+                  ."</node>". PHP_EOL;
+}
+
+function add_edge($from, $to) {
+
+          echo "<edge id='$from$to' source='$from' target='$to' />". PHP_EOL;
+
+}
+
 $nodeID = (isset($_REQUEST['node_id']) ? $_REQUEST['node_id'] : "");
-$xml = file_get_contents(local_url() . "ngapi.xml.php?node_id=" . urlencode(stripslashes($selectedNodeID)));
+if ($nodeID == "" || $nodeID == "[node_id]") {
+    ?>
+    Network Graph API supports the following central node types:
+    "agency"
+    "supplier"
+    "category"
+    "lobbyist"
+    "lobbyistclient"
+    "donationrecipient"
+    <?php
+    die();
+}
+     header('Content-Type: application/gexf+xml');
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" xmlns:viz="http://www.gexf.net/1.2draft/viz" version="1.2">
+    <meta lastmodifieddate="2009-03-20">
+        <creator>Gexf.net</creator>
+        <description>A hello world! file</description>
+    </meta>
+    <graph mode="static" defaultedgetype="directed">
+
+        <nodes>'. PHP_EOL;
+$url = local_url() . "ngapi.xml.php?node_id=" . urlencode(stripslashes($nodeID));
+$xml = file_get_contents($url);
 $graph = new SimpleXMLElement($xml);
-$name = $graph->xpath('//name');
-
-include_header($name[0]);
-
-echo "<!--". local_url() . "ngapi.xml.php?node_id=" . urlencode(stripslashes($selectedNodeID)) . "-->";
-echo '<p>Click on nodes to expand the network, hover over nodes, or lines joining nodes, to display more 
-information.</p>';
-echo '<div class="msg_list">
-<p class="msg_head">';
-echo $name[0];
-echo ' </p>
-<div class="msg_body"><ul>';
 $nodes = Array();
 foreach ($graph->xpath('//node') as $node) {
     $id = (string) $node['id'];
     if (strstr($id, "donor-"))
         continue;
     else {
-        $nodes[$id] = (string) $node['label'];
-        if (strstr($nodes[$id], ":")) {
+        $label = (string) $node['label'];
+        if (strstr($label, ":")) {
             $labelparts = explode(":", $nodes[$id]);
             array_shift($labelparts);
-            $nodes[$id] = trim(implode($labelparts));
+            $label = trim(implode($labelparts));
         }
+        add_node($id, $label);
     }
 }
+echo '</nodes>
+        <edges>'. PHP_EOL;
 foreach ($graph->xpath('//edge') as $edge) {
-    $message = $edge['tooltip'];
-    $message = str_replace($nodes[(string) $edge['head_node_id']], '<a href="?node_id=' . (string) $edge['head_node_id'] . '">' . htmlspecialchars($nodes[(string) $edge['head_node_id']]) . "</a>", $message);
-    $message = str_replace($nodes[(string) $edge['tail_node_id']], '<a href="?node_id=' . (string) $edge['tail_node_id'] . '">' . htmlspecialchars($nodes[(string) $edge['tail_node_id']]) . "</a>", $message);
-    echo "<li>$message</li>";
-}?>
+    add_edge($edge['head_node_id'],$edge['tail_node_id']);
+            }
+echo ' </edges>
+    </graph>
+</gexf>'. PHP_EOL;
+
+?>
