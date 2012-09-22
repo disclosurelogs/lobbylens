41004399b1e5672c3a61361860d37f4fd0528ed7
Maxious <maxious@lambdacomplex.org>
2012-09-22 21:51:02 +1000

    Readd index page!

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include	("libs/config.php");
+include_header();
+?>
+<div id="entry"><a href="networkgraph.php">Launch into the network graph</a></div>
+<!--<h1>News</h1>-->
+<?php
+include_footer();
+?>
