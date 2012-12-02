69d075fd05e484a70d1cee78f2f5d816f1516474
Maxious <maxious@lambdacomplex.org>
2012-12-02 19:40:45 +1100

    begin state import

--- admin/import_lobbyists.php
+++ admin/import_lobbyists.php
@@ -1,7 +1,7 @@
 <?php
 
 include "../libs/config.php";
-//$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
 $state_datasets = Array(
     "SA" => "South Australian Lobbyist Register",
@@ -39,7 +39,7 @@ function add_lobbyist($state, $abn, $business_name, $trading_name) {
             die();
         } else {
             echo ".";
-            set_time_limit(10);
+            set_time_limit(30);
         }
     } else {
         echo "exists @ ID: " . $lobbyistID . "<br>";
@@ -53,7 +53,7 @@ function add_lobbyist($state, $abn, $business_name, $trading_name) {
             die();
         } else {
             echo ".";
-            set_time_limit(10);
+            set_time_limit(30);
         }
     }
     if ($lobbyistID === 0)
@@ -72,7 +72,7 @@ function find_lobbyist($abn, $business_name, $trading_name) {
         $findlobbyist->execute(Array($business_name, $trading_name));
     }
     $err = $dbConn->errorInfo();
-    echo $findlobbyist->rowCount() . " rows found <br>\n";
+    //echo $findlobbyist->rowCount() . " rows found <br>\n";
     if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
         echo $query . " failed relation insert.<br>";
         print_r($err);
@@ -83,125 +83,201 @@ function find_lobbyist($abn, $business_name, $trading_name) {
             return NULL;
         } else {
             $lobbyist = $findlobbyist->fetch(PDO :: FETCH_ASSOC);
-            print_r($lobbyist);
+            //print_r($lobbyist);
             echo "found " . $lobbyist['lobbyistID'] . " <br>\n";
-            set_time_limit(10);
+            set_time_limit(30);
             return $lobbyist['lobbyistID'];
         }
     }
 }
 
-function add_client($lobbyistID, $clientName) {
-    $searchName = cleanseName($business_name);
-    echo "client: $business_name (searched as '$searchName')<br>";
+function find_lobbyist_by_name($name) {
+    global $dbConn;
+    echo "looking for lobbyist $name <br> \n";
+
+    $findlobbyist = $dbConn->prepare('SELECT "lobbyistID" from lobbyists where business_name = ? OR trading_name = ?;');
+    $findlobbyist->execute(Array($name, $name));
+
+    $err = $dbConn->errorInfo();
+    //echo $findlobbyist->rowCount() . " rows found <br>\n";
+    if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
+        echo $query . " failed searcn.<br>";
+        print_r($err);
+        die();
+    } else {
+        if ($findlobbyist->rowCount() == 0) {
+            echo "not found <Br>\n";
+            return NULL;
+        } else {
+            $lobbyist = $findlobbyist->fetch(PDO :: FETCH_ASSOC);
+            //print_r($lobbyist);
+            echo "found " . $lobbyist['lobbyistID'] . " <br>\n";
+            set_time_limit(30);
+            return $lobbyist['lobbyistID'];
+        }
+    }
+}
+
+function add_client($state, $clientName) {
+    global $dbConn;
+    $searchName = "%" . cleanseName($clientName) . "%";
+    echo "client: $clientName (searched as '$searchName')<br>";
     flush();
     // search for existing abn via name
-    $query = "SELECT lobbyistClientID, abn from lobbyist_clients where business_name = '" . mysql_real_escape_string($business_name) . "' OR business_name LIKE '%" . mysql_real_escape_string($searchName) . "%';";
-    $existresult = mysql_query($query);
+
+    $findclient = $dbConn->prepare('SELECT "lobbyistClientID", "ABN" from lobbyist_clients where business_name = ? OR business_name LIKE ?');
+    $findclient->execute(Array($clientName, $searchName));
+    $err = $dbConn->errorInfo();
+    if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
+            echo " bcvcvbcbv.<br>";
+            print_r($err);
+            die();
+        }
     $clientID = 0;
     $abn = 0;
-    if (mysql_num_rows($existresult) == 0) {
+    if ($findclient->rowCount() == 0) {
         // if name did not match.
-        $query = "SELECT supplierABN from supplierDetails where supplierName LIKE '%" . mysql_real_escape_string($searchName) . "%';";
-        $result = mysql_query($query);
-        if ($result) {
-            $row = mysql_fetch_row($result);
-            if ($row[1] > 0)
-                $abn = $row[1];
+        $findsupplier = $dbConn->prepare('SELECT "supplierABN" from supplierDetails where "supplierName" LIKE ?;');
+        $findsupplier->execute(Array($searchName));
+$err = $dbConn->errorInfo();
+if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
+            echo "fghfghfghf.<br>";
+            print_r($err);
+            die();
+        }
+        if ($findsupplier->rowCount() != 0) {
+            $row = $findsupplier->fetch(PDO :: FETCH_ASSOC);
+            if ($row['supplierABN'] != null && $row['supplierABN'] > 0) {
+                $abn = $row['supplierABN'];
+            }
         }
         if ($abn == 0) {
-            $abn = abnLookup($business_name);
+            // lookup online 
+            //set_time_limit(30);
+            $abn = abnLookup($clientName);
+        }
+        $findclientbyABN = $dbConn->prepare('SELECT "lobbyistClientID" from lobbyist_clients where "ABN" = ?;');
+        $findclientbyABN->execute(Array($abn));
+        $err = $dbConn->errorInfo();
+        if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
+            echo " dfgdfgdf.<br>";
+            print_r($err);
+            die();
+        }
+        if ($findclientbyABN->rowCount() != 0) {
+            $row = $findclientbyABN->fetch(PDO :: FETCH_ASSOC);
+            $clientID = $row['lobbyistClientID'];
         }
     } else {
         // found ABN or clientID
-        $row = mysql_fetch_row($existresult);
-        $clientID = $row[0];
-        $abn = $row[1];
+        $row = $findclient->fetch(PDO :: FETCH_ASSOC);
+        $clientID = $row['lobbyistClientID'];
+        $abn = $row['ABN'];
     }
+
     if ($clientID == 0) {
         // search for existing clientID
-        $query = "SELECT lobbyistClientID from lobbyist_clients where abn = $abn;";
-        $existIDresult = mysql_query($query);
-        if (mysql_num_rows($existIDresult) == 0) {
-            $query = "INSERT INTO lobbyist_clients (business_name, abn, $state)
-          VALUES (\"$business_name\"," . (float) $abn . ",'True');";
-            $result = mysql_query($query);
-            if (!$result) {
-                echo $query . " failed insert.<br>" . mysql_error();
-            } else {
-                $clientID = mysql_insert_id();
-                echo "is new client #$clientID <br>";
-            }
+        echo "$clientName, $abn";
+        $insclient = $dbConn->prepare('INSERT INTO lobbyist_clients (business_name, "ABN", ' . $state . ')
+          VALUES (?,?,\'True\') RETURNING "lobbyistClientID";');
+        $insclient->bindParam(1, $clientName);
+        $insclient->bindParam(2, $abn);
+        $insclient->execute();
+        /*        echo "$clientName, $abn";
+        $query = 'INSERT INTO lobbyist_clients (business_name, "ABN", ' . $state . ')
+          VALUES ('.$dbConn->quote($clientName).','.$dbConn->quote($abn).',\'True\') RETURNING "lobbyistClientID";';
+        echo $query;
+        $insclient = $dbConn->exec($query);*/
+        $err = $dbConn->errorInfo();
+        if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
+            echo " failed client insert.<br>";
+            print_r($err);
+            die();
         } else {
-            $row = mysql_fetch_array($existIDresult);
-            $clientID = $row[0];
+            $result = $insclient->fetch(PDO::FETCH_ASSOC);
+
+            echo "is new client #" . $result['lobbyistClientID'] . " <br>";
+            set_time_limit(30);
+            return $result['lobbyistClientID'];
         }
+    } else {
+        echo "exists @ ID: " . $clientID . "<br>";
+        return $clientID;
     }
-    echo "exists @ ID: " . $clientID . "<br>";
 }
 
 function add_relationship($state, $lobbyistID, $clientID) {
-    $stateupdate = $dbConn->prepare("UPDATE lobbyist_clients  SET ?='True' WHERE lobbyistClientID = ?;");
-    $stateupdate->execute(Array($state, $clientID));
+    global $dbConn;
+    $stateupdate = $dbConn->prepare('UPDATE lobbyist_clients  SET ' . $state . ' =\'True\' WHERE "lobbyistClientID" = ?;');
+    $stateupdate->execute(Array($clientID));
     $err = $dbConn->errorInfo();
     if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
-        echo $query . " failed client state update.<br>";
+        echo " failed client state update.<br>";
         print_r($err);
         die();
     } else {
         echo ".";
-        set_time_limit(10);
+        set_time_limit(30);
     }
 
 
-    if ($clientID == 0 and $abn == 0) {
+    if ($clientID == 0 || $lobbyistID == 0) {
         echo "<br><b>Manual intervention required for client $clientID in relationship with lobbyist $lobbyistID</b><br>";
+        
     } else {
 
-        $relupdate = $dbConn->prepare("INSERT INTO lobbyist_relationships (lobbyistID, lobbyistClientID)
-        VALUES (?,?);");
+        $relupdate = $dbConn->prepare('INSERT INTO lobbyist_relationships ("lobbyistID", "lobbyistClientID")
+        VALUES (?,?);');
         $relupdate->execute(Array($lobbyistID, $clientID));
         $err = $dbConn->errorInfo();
         if ($err[2] != "" && strpos($err[2], "duplicate key") === false) {
-            echo $query . " failed relation insert.<br>";
+            echo " failed relation insert.<br>";
             print_r($err);
             die();
         } else {
             echo ".";
-            set_time_limit(10);
+            set_time_limit(30);
         }
     }
 }
 
 // federal parser
 $datasetName = "Federal Government Lobbyists Register";
-$lobbyist_clients = json_decode(file_get_contents("clients.json"));
-$lobbyists = json_decode(file_get_contents("lobbyists.json"));
-//$lobbyist_clients = json_decode(getPage('https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=australian-government-register-of-lobbyists&query=select+*+from+`lobbyist_clients`&apikey='));
-//
-//$lobbyists = json_decode(getPage('https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=australian-government-register-of-lobbyists&query=select+*+from+`lobbyists`&apikey='));
-foreach ($lobbyists as $lobbyist) {
+//$lobbyist_clients = json_decode(file_get_contents("clients.json"));
+//$lobbyists = json_decode(file_get_contents("lobbyists.json"));
+/*foreach ($lobbyists as $lobbyist) {
+ $lobbyist_clients = json_decode(getPage('https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=australian-government-register-of-lobbyists&query=select+*+from+`lobbyist_clients`&apikey='));
+$lobbyists = json_decode(getPage('https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=australian-government-register-of-lobbyists&query=select+*+from+`lobbyists`&apikey='));
+
     $lobbyistID = add_lobbyist("federal", str_replace(" ", "", $lobbyist->abn), $lobbyist->business_entity_name, $lobbyist->trading_name);
 }
-die();
+
 foreach ($lobbyist_clients as $lobbyist_client) {
-    $lobbyistID = find_lobbyist($lobbyist_client->lobbyist_name);
-    $clientID = add_client($lobbyistID, $lobbyist_client->client_name);
-    add_relationship($lobbyistID, $clientID);
+    $lobbyistID = find_lobbyist_by_name($lobbyist_client->lobbyist_name);
+    $clientID = add_client("federal", $lobbyist_client->client_name);
+
+    add_relationship("federal", $lobbyistID, $clientID);
 }
+die();*/
 // state parsers
 foreach ($state_urls as $state => $url) {
     if ($url == "") {
-        echo "Skipping $datasetName due to no URL<br>\n";
+        echo "Skipping $state due to no URL<br>\n";
         continue;
-    }
+    } else {
     $lobbyists = json_decode(getPage($url));
     foreach ($lobbyists as $lobbyist) {
-        $lobbyistID = add_lobbyist(strtolower($state), $lobbyist->abn, $lobbyist->business_name, $lobbyist->trading_name);
-        foreach ($lobbyist->clients as $client) {
-            $clientID = add_client($lobbyistID, $client);
+        $abn = str_replace("No A.B.N","",$lobbyist->abn);
+        $lobbyistID = add_lobbyist(strtolower($state), $abn, $lobbyist->business_name, $lobbyist->trading_name);
+        //print_r($lobbyist->clients);
+        echo $lobbyist->clients;
+        foreach (json_decode($lobbyist->clients) as $client) {
+            echo $client;
+            $clientID = add_client(strtolower($state), $client);
             add_relationship(strtolower($state), $lobbyistID, $clientID);
         }
+    }
+    die();
     }
 }
 ?>
